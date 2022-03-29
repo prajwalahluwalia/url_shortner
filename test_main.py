fc6fdfe4d29729412a2e2ccd35cab4385fb7e6c8
prajwalahluwalia <69308535+prajwalahluwalia@users.noreply.github.com>
2022-03-29 21:24:51 +0530

    Add files via upload

---
+++ test_main.py
@@ -0,0 +1,7 @@
+from cgitb import reset
+from urllib import response
+from urlshort import create_app
+
+def test_shorten(client):
+    response = client.get('/')
+    assert b'Shorten' in response.data
