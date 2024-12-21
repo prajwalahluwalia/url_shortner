fb57cbec2a37318de379172f8454c1c95acc416f
prajwalahluwalia <69308535+prajwalahluwalia@users.noreply.github.com>
2024-12-21 13:07:36 +0530

    Update __init__.py

--- code/__init__.py
+++ code/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app(test_config = None):
-    app =Flask(__name__)
+db = SQLAlchemy()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
     app.secret_key = "klsgciqegifgelbilug;o"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://username:password@localhost/urlshortener"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
 
     from . import urlshort
     app.register_blueprint(urlshort.bp)
+
+    with app.app_context():
+        db.create_all()
 
     return app
